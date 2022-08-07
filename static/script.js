a7b680c02300d152ac91bc470ecbe4065564da4c
Samarth M <samarthm@Samarths-MacBook-Air.local>
2022-08-07 18:50:11 +0530

    Finished exercise 1 and 2

---
+++ static/script.js
@@ -0,0 +1,30 @@
+function getAge()
+{
+    let bornYear = prompt("What year were you born in?")
+    const d = new Date();
+    let currYear = d.getFullYear();
+    ageInDays = (currYear - bornYear) * 365;
+
+    let resultElement = document.getElementById("age");
+    resultElement.innerHTML="You are "+ ageInDays +" days old"
+}
+
+function reset(){
+    let resultElement = document.getElementById("age");
+    resultElement.innerHTML="You are _____ days old"
+}
+
+x=200
+y=200
+
+function addCat(){
+    let catsElement = document.getElementById("cats");
+    let catImg = document.createElement("img");
+    catImg.src="https://placekitten.com/"+x+"/"+y;
+    y+=1
+    catImg.height=200;
+    catImg.width=200;
+    catImg.style.boxShadow = "0px 10px 50px rgba(0,0,0,0.7)";
+    catsElement.append(catImg);
+}
+
